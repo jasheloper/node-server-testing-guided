bb159bd01135f29901eeef9d04654c6ed751be4e
Jashele Tillman <48870048+jasheloper@users.noreply.github.com>
2019-12-14 11:12:21 -0700

    added tests for the server

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,3 +1,29 @@
-test('true', () => {
-   expect(true).toBe(true)
-})
+const server = require("./server.js");
+const request = require("supertest"); //supertest tests multiple endpoints @ same time & on different ports
+
+// EACH TEST SHOULD ONLY TEST 1 THING   : BEST PRACTICE
+
+describe("server.js", () => {
+  // we are testing   :   that our environment is the testing environment
+
+  test("should be the testing environment", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+
+  describe("GET /", () => {
+    it("should return 200 OK", async () => {
+      const res = await request(server).get("/");
+      expect(res.status).toBe(200);
+    });
+
+    it("should be json", async () => {
+      const res = await request(server).get("/");
+      expect(res.type).toBe("application/json");
+    });
+
+    it("should return the right object", async () => {
+      const res = await request(server).get("/");
+      expect(res.body).toEqual({ api: "up" });
+    });
+  });
+});
--- notes.js
+++ notes.js
@@ -7,3 +7,15 @@
 // https://jestjs.io/docs/en/api
 
 // npx jest 
+
+
+// test script to run test ... 
+      // "test": "cross-env DB_ENV=testing jest --watch",  ....in pckg json
+         // ensure u have environment variable set ... 
+         // we can manually set it 
+         // don't worry about operating system ... cross-env DB_ENV=testing ... allows you to specify what env variable you want to set & it takes care of doing it the right way for the OS you're running
+
+                  // yarn add cross-env 
+
+// yarn add -D supertest
+             
